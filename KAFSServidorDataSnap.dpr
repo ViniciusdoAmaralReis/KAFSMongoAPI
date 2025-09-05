e99a2ac4e89d57d73a802e6ffc79069a4870b824
Vinicius do Amaral Reis <viniciusdoamaralreis@gmail.com>
2025-09-05 18:08:18 -0300

    Create KAFSServidorDataSnap.dpr

---
+++ KAFSServidorDataSnap.dpr
@@ -0,0 +1,22 @@
+program KAFSServidorDataSnap;
+
+{$APPTYPE CONSOLE}
+
+uses
+  System.SysUtils,
+  uServerMethods in 'uServerMethods.pas',
+  uServerContainer in 'uServerContainer.pas' {ServerContainer: TDataModule},
+  uKAFSFuncoes in '..\uKAFSFuncoes\uKAFSFuncoes.pas',
+  uKAFSMongoDB in '..\uKAFSMongoDB\uKAFSMongoDB.pas',
+  uKAFSConexaoMongoDBAtlas in '..\TKAFSConexaoMongoDBAtlas\uKAFSConexaoMongoDBAtlas.pas';
+
+begin
+  try
+    RunDSServer;
+    ReadLn;
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end
+end.
+
